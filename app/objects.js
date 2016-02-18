3479566d99b3b5aabf5401d98dc2f4c97872e2ac
John Bartos <jbartos7@gmail.com>
2016-02-18 16:29:41 -0500

    Complete objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,15 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
-  }
+    return Object.keys(obj).map(key => {
+      return key + ': ' + obj[key];
+    });
 };
